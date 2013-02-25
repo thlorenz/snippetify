61d6d86742ec860381e7ce0a0a56a866bd212e5e
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-25 07:47:24 -0500

    pulling main script over

---
+++ snippetify.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var parse = require('esprima').parse;
+
+module.exports = function snippetify(script) {
+  var snippets = []
+    , lines = script.split('\n')
+    , lineno = 0;
+
+  function nextChunk(code) {
+    lineno++;
+    var nextLine = lines.shift();
+
+    code = code + nextLine;
+    try {
+      parse(code);
+      return code;
+    } catch(e) {
+      return nextChunk(code + '\n');
+    }
+  }
+
+  while (lines && lines.length) {
+    var snippetStart = lineno + 1
+      , snippet = nextChunk('');
+
+    snippets.push({ start: snippetStart, end: lineno, code: snippet });
+  }
+  return { chunks: chunks, lines: script.split('\n') };
+};
