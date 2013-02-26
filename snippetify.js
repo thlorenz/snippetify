7c9aa8a7928ccde8a6ba2cb62f60e02cf09bec8d
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-26 08:38:33 -0500

    handling more snippets
    - multi line assignments work
    - commafirst assignments fixed to treat them as single line snippet
    - ast attached to result
    - fixed code and raw code attached to result

--- snippetify.js
+++ snippetify.js
@@ -1,30 +1,60 @@
 'use strict';
 
-var parse = require('esprima').parse;
+var parse = require('esprima').parse
+  , commaFirstAssignment = /^[\t ]*,[\t ]*\S[\t ]*=[\t ]*\S+/;
 
-module.exports = function snippetify(script) {
+function singleLineFix(line) {
+  var fixed = line;
+
+  if (commaFirstAssignment.test(line)) {
+    var idx = line.indexOf(',');
+    fixed = fixed.slice(0, idx) + ' ' + fixed.slice(idx + 1);
+  }
+
+  return fixed;
+}
+
+var s = module.exports = function snippetify(script) {
   var snippets = []
-    , lines = script.split('\n')
-    , lineno = 0;
+    , lines = script.split('\n');
 
-  function nextChunk(code) {
-    lineno++;
-    var nextLine = lines.shift();
+  if (!lines.length) return [];
 
-    code = code + nextLine;
+  function nextChunk(code, raw) {
+    var singleLine =  code.length ===  0
+      , line       =  lines[lineno] 
+      , fixed      =  singleLine ? singleLineFix(line) : line;
+
+    raw = line + raw;
+    code = fixed + code;
+
     try {
-      parse(code);
-      return code;
+      return { code: code, raw: raw, ast: parse(code) };
     } catch(e) {
-      return nextChunk(code + '\n');
+      if (--lineno === -1) throw new Error('unable to snippetify ' + code);
+      return nextChunk('\n' + code, '\n' + raw);
     }
   }
 
-  while (lines && lines.length) {
-    var snippetStart = lineno + 1
-      , snippet = nextChunk('');
+  var lineno = lines.length;
 
-    snippets.push({ start: snippetStart, end: lineno, code: snippet });
+  while (--lineno > -1) {
+    var snippetEnd = lineno +1
+      , snippet = nextChunk('', '')
+      , snippetStart = lineno + 1;
+
+    snippets.unshift({
+        start :  snippetStart
+      , end   :  snippetEnd
+      , code  :  snippet.code
+      , raw   :  snippet.raw
+      , ast   :  snippet.ast
+    });
   }
+
   return snippets;
 };
+
+/*console.log(
+  s('var o = {\n    a: 1\n  , b: 2  , c: 3} ')
+);*/
--- test/assignments.js
+++ test/assignments.js
@@ -1,10 +1,11 @@
 'use strict';
 /*jshint asi: true */
 
-var test = require('trap').test
-  , highlight = require('cardinal').highlight
-  , diff = require('difflet')({ indent: 2, comma: 'first'})
-  , snippetify = require('..')
+var test       =  require('trap').test
+  , styles     =  require('ansistyles')
+  , highlight  =  require('cardinal').highlight
+  , diff       =  require('difflet')({ indent: 2, comma: 'first'})
+  , snippetify =  require('..')
 
 function inspect(obj, depth) {
   return require('util').inspect(obj, false, depth || 5, true)
@@ -19,10 +20,13 @@ function check(msg, fn, expected, trace) {
   var code = lines.join('\n')
     , result = snippetify(code)
 
+  // don't include ast or fixed code in comparison
+  result = result.map(function (x) { return { start: x.start, end: x.end, raw: x.raw } })
+
   if (trace) console.log(diff.compare({}, result))
 
-  test('snippetifying', function (t) {
-    t.deepEqual(result, expected, '\n' + highlight(code, { linenos: true }) + '\n' + inspect(expected))
+  test(msg + styles.reset('\n') + highlight(code, { linenos: true }), function (t) {
+    t.deepEqual(result, expected, styles.reset('\n') + inspect(expected))
   })
 }
 
@@ -31,7 +35,7 @@ check(
   , function _() {
 var a = 3;
     }
-  , [{"start":1,"end":1,"code":"var a = 3;"}]
+  , [{"start":1,"end":1,"raw":"var a = 3;"}]
 )
 
 check(
@@ -40,13 +44,43 @@ check(
 var a = 3;
 var b = 2;
     }
-  , [ 
-      { "start" : 1
+  , [ { "start" : 1
       , "end" : 1
-      , "code" : "var a = 3;" }
+      , "raw" : "var a = 3;" }
     , 
       { "start" : 2
       , "end" : 2
-      , "code" : "var b = 2;" }
+      , "raw" : "var b = 2;" }
     ]
 )
+
+check(
+    'two var assignments comma first'
+  , function _() {
+var a = 3
+  , b = 2;
+    }
+  , [ { "start" : 1
+      , "end" : 1
+      , "raw" : "var a = 3" }
+    , 
+      { "start" : 2
+      , "end" : 2
+      , "raw" : "  , b = 2;" }
+    ]
+)
+
+check(
+    'multiline object literal assignment'
+  , function _() {
+var o = {
+  a : 1,
+  b : 2,
+  c : 3 };
+    }
+  , [ { "start" : 1
+      , "end" : 4
+      , "raw" : "var o = {\n  a : 1,\n  b : 2,\n  c : 3 };" }
+    ]
+)
+
